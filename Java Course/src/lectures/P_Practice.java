7ccdc6f004c98a7ce3d3057ed284fc060de62af7
CHESTER\chuan <chuan.me@gmail.com>
2019-05-11 12:27:30 +0800

    add P_Practice

---
+++ Java Course/src/lectures/P_Practice.java
@@ -0,0 +1,21 @@
+package lectures;
+
+public class P_Practice {
+
+     
+     public static void 判斷式練習() {
+          
+     }
+     
+     public static void 陣列for內容交換練習() {
+          // swap 演算法
+     }
+     
+     public static void for迴圈練習() {
+          
+     }
+     
+     public static void 遞迴函示練習() {
+          
+     }
+}
