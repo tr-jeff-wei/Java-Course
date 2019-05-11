efe9c09246b65377a891d77f4b8d039f84f12c3e
CHESTER\chuan <chuan.me@gmail.com>
2019-05-12 02:53:54 +0800

    u

--- Java Course/src/homework/hw_0511.java
+++ Java Course/src/homework/hw_0511.java
@@ -1,16 +1,45 @@
 package homework;
 
+import java.util.Scanner;
 
 public class hw_0511 {     
      
-     public static void main(String[] args) { 
+     /*
+      * 請問在使用者輸入下列訊息後，程式會印出什麼
+      * -------------[輸入訊息]---------------
+      * 第一行輸入：  3
+      * 第二行輸入：  AB
+      * 
+      */
+     public static void main(String[] args) {
+          
+          Scanner sc = new Scanner(System.in) ;
+          
+          String n1string = sc.nextLine() ;
+          int n1 = Integer.parseInt(n1string);
+          
+          String text = sc.nextLine();
+          
+          for( int i=0 ; i<n1 ; i++) {
+               System.out.println(text+",");
+          }
+          
+     }
+     
+     
+     
+     
+     
+     
+     
+     public static void main4(String[] args) { 
           // 下列程式將輸出什麼內容 
           show(5) ;
      }
      
      public static void show(int k) {
           System.out.println(k);
-          if( k > 3) {
+          if( k >= 3) {
                show( k-2 );
           }else {
                System.out.println("end!!");
