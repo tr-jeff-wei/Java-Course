b1e3a8dfed145adef1ec63a7c357117f0e02849f
CHESTER\chuan <chuan.me@gmail.com>
2019-06-14 08:43:52 +0800

    update

---
+++ Java Course/src/lectures/P_Utility_practice.java
@@ -0,0 +1,35 @@
+package lectures;
+
+public class P_Utility_practice {
+
+
+    public static void q1(){
+
+        // 運用程式計算下列字串的數字和
+        String t = "89,90,100,70,80" ;
+
+    }
+
+    public static void q2(){
+
+        // 運用程式計算下列字串的數字，有幾個數字不及格（<=60）
+        String t = "89,90,100,70,80" ;
+
+    }
+
+    public static void q3(){
+
+        // 運用 indexOf 找到 Google 文字位置，將 Google 換成 ******
+        String t = "受不了一直被爆雷！Google 直接發佈 Pixel 4產品圖" ;
+
+
+    }
+
+    public static void q4(){
+
+        // 取得 100~1000 之間的亂數
+
+
+    }
+
+}
