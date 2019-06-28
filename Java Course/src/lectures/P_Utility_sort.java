dc9c857b6b3317c9a1759832add35ee6690d78ad
CHESTER\chuan <chuan.me@gmail.com>
2019-06-28 14:18:53 +0800

    update

--- Java Course/src/lectures/P_Utility_sort.java
+++ Java Course/src/lectures/P_Utility_sort.java
@@ -6,6 +6,7 @@ public class P_Utility_sort {
 
     public static void main(String[] args) {
         sort_array();
+        sort_arry_obj();
     }
 
 
@@ -31,4 +32,40 @@ public class P_Utility_sort {
     }
 
 
+    public static void sort_arry_obj(){
+
+        Student[] students = new Student[4] ;
+        students[0] = new Student("Joe" , 60 , 85 ) ;
+        students[1] = new Student("Mary" , 70 , 73 ) ;
+        students[2] = new Student("Frank" , 90 , 60 ) ;
+        students[3] = new Student("Annie" , 80 , 90 ) ;
+
+        Arrays.sort(students);
+        System.out.println(Arrays.toString(students));
+
+
+    }
+}
+
+
+class Student implements Comparable<Student>{
+    String name ;
+    int java ;
+    int math ;
+
+    public Student(String name, int java, int math) {
+        this.name = name;
+        this.java = java;
+        this.math = math;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        return this.math - other.math ;
+    }
+
+    @Override
+    public String toString() {
+        return name+" m:"+math+"  j:"+java ;
+    }
 }
