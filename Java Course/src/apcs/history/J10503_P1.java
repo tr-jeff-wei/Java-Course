a0e307fab26d0aca4ce308fd43f3b26a1d5fdc3a
CHESTER\chuan <chuan.me@gmail.com>
2019-07-06 10:35:05 +0800

    update

--- Java Course/src/apcs/history/J10503_P1.java
+++ Java Course/src/apcs/history/J10503_P1.java
@@ -5,7 +5,83 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class J10503_P1 {
+
+
     public static void main(String[] args) {
+
+        int nx = 3 ;
+        int ny = 6 ;
+        int[][] table = new int[nx][ny] ;
+
+        for (int x = 0; x < nx; x++) {
+            for (int y = 0; y < ny; y++) {
+                table[x][y] =(int) (Math.random()*10);
+            }
+        }
+
+        showTable(table);
+
+        //-----------------------
+        int tY=0 ;
+        int bY=ny-1;
+        while (tY<bY) {
+            for (int x = 0; x < table.length; x++) {
+
+                // swap
+                int tmp = table[x][tY] ;
+                table[x][tY] = table[x][bY] ;
+                table[x][bY] = tmp ;
+
+            }
+            tY++ ;
+            bY-- ;
+        }
+
+        showTable(table);
+
+
+
+
+    }
+
+    static void showTable( int[][] table ){
+        System.out.println("===================");
+        for (int y = 0; y < table[0].length; y++) {
+            for (int x = 0; x < table.length; x++) {
+                System.out.print(table[x][y]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("===================");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void test(String[] args) {
 
 
         String input = "3 2 3\n" +
