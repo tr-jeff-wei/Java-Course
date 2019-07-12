283b55f6a937df31016a2432cfbf689573c01c55
CHESTER\chuan <chuan.me@gmail.com>
2019-07-12 16:13:03 +0800

    update

--- Java Course/src/apcs/history/J10503_P1.java
+++ Java Course/src/apcs/history/J10503_P1.java
@@ -2,101 +2,71 @@ package apcs.history;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class J10503_P1 {
 
-
     public static void main(String[] args) {
 
-        int nx = 3 ;
-        int ny = 6 ;
-        int[][] table = new int[nx][ny] ;
 
-        for (int x = 0; x < nx; x++) {
-            for (int y = 0; y < ny; y++) {
-                table[x][y] =(int) (Math.random()*10);
-            }
-        }
+        String input ="";
 
-        showTable(table);
+//        input = "10\n" +
+//                "0 11 22 33 55 66 77 99 88 44\n";
+        input = "1\n" +
+                "66\n";
+//        input = "1\n"+
+//                "13\n";
 
-        //-----------------------
-        int tY=0 ;
-        int bY=ny-1;
-        while (tY<bY) {
-            for (int x = 0; x < table.length; x++) {
-
-                // swap
-                int tmp = table[x][tY] ;
-                table[x][tY] = table[x][bY] ;
-                table[x][bY] = tmp ;
-
-            }
-            tY++ ;
-            bY-- ;
-        }
-
-        showTable(table);
-
-
-
-
-    }
-
-    static void showTable( int[][] table ){
-        System.out.println("===================");
-        for (int y = 0; y < table[0].length; y++) {
-            for (int x = 0; x < table.length; x++) {
-                System.out.print(table[x][y]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println("===================");
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public static void test(String[] args) {
-
-
-        String input = "3 2 3\n" +
-                "1 1\n" +
-                "3 1\n" +
-                "1 2\n" +
-                "1 0 0\n" +
-                "";
-
-        InputStream is = new ByteArrayInputStream( input.getBytes()) ;
+        InputStream is = new ByteArrayInputStream(input.getBytes());
         System.setIn(is);
 
-        Scanner sc= new Scanner(System.in) ;
 
+        Scanner sc = new Scanner(System.in) ;
+        int n = sc.nextInt();
+        int[] data = new int[n] ;
+        for (int i = 0; i < n; i++) {
+            data[i] = sc.nextInt();
+        }
 
+        Arrays.sort(data);
+        
+        int max =-1 ;
+        int min =-1 ;
+        for (int i = 0; i < data.length; i++) {
+            if( data[i]>=60){
+                // 及格
+                if( min==-1 || data[i]<min ){
+                    min = data[i] ;
+                }
+            }else{
+                // 不及格
+                if( max==-1 || data[i]>max ){
+                    max = data[i] ;
+                }
+            }
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            if(i==0){
+                System.out.print(data[i]);
+            }else{
+                System.out.print(" "+data[i]);
+            }
+        }
+        System.out.println();
+
+        if( max == -1){
+            System.out.println("best case");
+        }else{
+            System.out.println(max);
+        }
+        if( min == -1){
+            System.out.println("worst case");
+        }else{
+            System.out.println(min);
+        }
 
     }
 }
---
+++ Java Course/src/apcs/history/J10503_P2.java
@@ -0,0 +1,125 @@
+package apcs.history;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class J10503_P2 {
+
+
+    public static void main(String[] args) {
+        String input = "";
+//                input = "3 2 3\n" +
+//                "1 1\n" +
+//                "3 1\n" +
+//                "1 2\n" +
+//                "1 0 0\n" +
+//                "";
+
+        input = "3 2 2\n" +
+                "3 3\n" +
+                "2 1\n" +
+                "1 2\n" +
+                "0 1\n" +
+                "";
+
+        InputStream is = new ByteArrayInputStream(input.getBytes());
+        System.setIn(is);
+
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt() ;
+        int c = sc.nextInt() ;
+        int m = sc.nextInt() ;
+        int[][] data = new int[r][c] ;
+        for (int ri = 0; ri < r; ri++) {
+            for (int ci = 0; ci < c; ci++) {
+                data[ri][ci] = sc.nextInt() ;
+            }
+        }
+//        showTable(data);
+
+        // 旋轉 , 翻轉
+        for (int i = 0; i < m; i++) {
+            int mi = sc.nextInt() ;
+            switch (mi){
+                case 0:
+                    data = rotateTable(data) ;
+                    break ;
+                case 1:
+                    swapTable(data);
+                    break;
+            }
+//            System.out.println("==>"+mi);
+//            showTable(data);
+        }
+        System.out.println( data.length+" "+data[0].length);
+        for (int ri = 0; ri < data.length; ri++) {
+            for (int ci = 0; ci < data[ri].length; ci++) {
+                System.out.print( data[ri][ci]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int[][] rotateTable(int[][] table) {
+        int[][] newTable = new int[table[0].length][table.length];
+
+        int nc = table.length-1 ;
+        for (int ri = 0; ri < table.length; ri++) {
+            int nr = 0 ;
+            for (int ci = 0; ci < table[ri].length; ci++) {
+                newTable[nr][nc]= table[ri][ci] ;
+                nr++ ;
+            }
+            nc-- ;
+        }
+
+
+        return newTable;
+
+    }
+
+    static void swapTable(int[][] table) {
+        int top = 0;
+        int bottom = table.length - 1;
+        while (top < bottom) {
+            for (int ci = 0; ci < table[0].length; ci++) {
+                // swap
+                int tmp = table[top][ci];
+                table[top][ci] = table[bottom][ci];
+                table[bottom][ci] = tmp;
+
+            }
+            top++;
+            bottom--;
+        }
+    }
+
+    static void showTable(int[][] table) {
+        System.out.println("===================");
+        for (int ri = 0; ri < table.length; ri++) {
+            for (int ci = 0; ci < table[ri].length; ci++) {
+                System.out.print(table[ri][ci]+" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("===================");
+    }
+
+    static int[][] genRandomTable() {
+        int nx = 3;
+        int ny = 6;
+        int[][] table = new int[nx][ny];
+
+        for (int x = 0; x < nx; x++) {
+            for (int y = 0; y < ny; y++) {
+                table[x][y] = (int) (Math.random() * 10);
+            }
+        }
+        return table;
+    }
+
+
+
+}
